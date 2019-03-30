403ddd42670ba084a853614b966affd10f0ac7d0
guodd369 <guodd369@163.com>
2019-03-30 13:15:13 +0800

    Initial commit

---
+++ open-admin/src/main/java/com/io/OpenAdminApplication.java
@@ -0,0 +1,13 @@
+package com.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAdminApplication.class, args);
+    }
+
+}
---
+++ open-generator/src/main/java/com/io/OpenGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenGeneratorApplication.class, args);
+    }
+
+}
