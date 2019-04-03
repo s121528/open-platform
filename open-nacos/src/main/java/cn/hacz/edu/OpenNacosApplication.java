00b78c16d7b406e6550a5930a7b6b26dcd1098a3
guodd369 <guodd369@163.com>
2019-04-03 13:06:53 +0800

    初始化项目

---
+++ open-nacos/src/main/java/cn/hacz/edu/OpenNacosApplication.java
@@ -0,0 +1,33 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * project -
+ *
+ * @author yanfa07
+ * @version 1.0
+ * @date 日期:2019/4/3 时间:9:37
+ * @JDK 1.8
+ * @Description 功能模块：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class OpenNacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenNacosApplication.class, args);
+    }
+
+    /**
+     * 功能描述：
+     */
+    @RequestMapping(value = "/info")
+    public String test() {
+        return "info";
+    }
+}
