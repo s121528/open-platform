0eecd1c0276c5e8caec01e88a3810b1f7b16dce6
guodd369 <guodd369@163.com>
2019-04-28 17:49:41 +0800

    加密处理

--- open-nacos/src/main/java/cn/hacz/edu/OpenNacosApplication.java
+++ open-nacos/src/main/java/cn/hacz/edu/OpenNacosApplication.java
@@ -2,7 +2,6 @@ package cn.hacz.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description 功能模块：
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 @RestController
 public class OpenNacosApplication {
     public static void main(String[] args) {
