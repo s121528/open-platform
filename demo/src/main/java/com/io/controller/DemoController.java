df192f040ae273efbadaf8d14ddc87562c18c81d
guodd369 <guodd369@163.com>
2019-05-10 13:23:01 +0800

    结构调整

--- demo/src/main/java/com/io/controller/DemoController.java
+++ demo/src/main/java/com/io/controller/DemoController.java
@@ -34,11 +34,12 @@ public class DemoController {
         invoiceInfoReqVo.setWaybillNum("1801010363760");
         invoiceInfoReqVo.setCompanyNum("1");
         String s3 = DESUtil.encode(JSONUtil.toJsonStr(invoiceInfoReqVo));
+
         RequestVo requestVo = new RequestVo();
         requestVo.setCompanyCode("01201901220939520001");
         requestVo.setRequestType("1");
         requestVo.setContent(s3);
-        String postForObject = mkRestTemplate.postForObject("http://42.236.7.2:8108/rms/invoice/invoiceInfos", requestVo, String.class);
+        String postForObject = mkRestTemplate.postForObject("http://127.0.0.1:8108/rms/invoice/invoiceInfos", requestVo, String.class);
         JSONObject jsonObject = JSONUtil.parseObj(postForObject);
         String message = DESUtil.decode(JSONUtil.toJsonStr(jsonObject.get("message")), "tjhj0xtvaqjicdu9os2agyu8", "3213abcd");
         System.out.println("====================下面===========");
